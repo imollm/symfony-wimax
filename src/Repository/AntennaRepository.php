1bcfeb3d949c0929cdcad5ea29c09817c7c9a96f
Ivan <ivanmoll07@gmail.com>
2020-09-21 22:25:19 +0200

    Create a antenna repository to do custom queries.

--- src/Repository/AntennaRepository.php
+++ src/Repository/AntennaRepository.php
@@ -2,10 +2,18 @@
 
 namespace App\Repository;
 
+use App\Entity\Antenna;
 use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ManagerRegistry;
 
-class AntennaRepository 
+class AntennaRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Antenna::class);
+    }
     public static function setNullUserId($connection, User $user)
     {
         $sql = "UPDATE antennas SET user_id = NULL WHERE user_id = " . $user->getId(); 
