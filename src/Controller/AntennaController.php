22f714be966baac4eb56d99746365b8444587615
Ivan <ivanmoll07@gmail.com>
2020-09-21 22:21:26 +0200

    Add index page of antennas.

--- src/Controller/AntennaController.php
+++ src/Controller/AntennaController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Antenna;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Flex\Unpack\Result;
 
 class AntennaController extends AbstractController
 {
     /**
-     * @Route("/antenna", name="antenna")
+     * @Route("/antennas", name="antennas")
      */
     public function index()
     {
+        $antennasRepo= $this->getDoctrine()->getRepository(Antenna::class);
+        $antennas = $antennasRepo->findAll();
+
         return $this->render('antenna/index.html.twig', [
-            'controller_name' => 'AntennaController',
+            'title' => 'Antennas',
+            'header' => 'Listado de antenas',
+            'antennas' => $antennas
         ]);
     }
 }
